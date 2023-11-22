6199a7ddd95bbc6c10d7ab9e86b036587331f57e
mrsaif888 <105549569+mrsaif888@users.noreply.github.com>
2023-11-21 20:29:20 -0800

    Changed the path of files

--- routes.php
+++ routes.php
@@ -1,24 +1,24 @@
 <?php
 
-$router->get('/', 'controllers/index.php');
-$router->get('/about', 'controllers/about.php');
-$router->get('/contact', 'controllers/contact.php');
+$router->get('wrap-demo','/', 'controllers/index.php');
+$router->get('wrap-demo','/about', 'controllers/about.php');
+$router->get('wrap-demo','/contact', 'controllers/contact.php');
 
-$router->get('/notes', 'controllers/notes/index.php')->only('auth');
-$router->get('/note', 'controllers/notes/show.php');
-$router->delete('/note', 'controllers/notes/destroy.php');
+$router->get('wrap-demo','/notes', 'controllers/notes/index.php')->only('auth');
+$router->get('wrap-demo','/note', 'controllers/notes/show.php');
+$router->delete('wrap-demo','/note', 'controllers/notes/destroy.php');
 
-$router->get('/note/edit', 'controllers/notes/edit.php');
-$router->patch('/note', 'controllers/notes/update.php');
+$router->get('wrap-demo','/note/edit', 'controllers/notes/edit.php');
+$router->patch('wrap-demo','/note', 'controllers/notes/update.php');
 
-$router->get('/notes/create', 'controllers/notes/create.php');
-$router->post('/notes', 'controllers/notes/store.php');
+$router->get('wrap-demo','/notes/create', 'controllers/notes/create.php');
+$router->post('wrap-demo','/notes', 'controllers/notes/store.php');
 
-$router->get('/register', 'controllers/registration/create.php')->only('guest');
-$router->post('/register', 'controllers/registration/store.php');
+$router->get('wrap-demo','/register', 'controllers/registration/create.php')->only('guest');
+$router->post('wrap-demo','/register', 'controllers/registration/store.php');
 
-$router->get('/login', 'controllers/session/create.php')->only('guest');
-$router->post('/session', 'controllers/session/store.php')->only('guest');
-$router->delete('/session', 'controllers/session/destroy.php')->only('auth');
+$router->get('wrap-demo','/login', 'controllers/session/create.php')->only('guest');
+$router->post('wrap-demo','/session', 'controllers/session/store.php')->only('guest');
+$router->delete('wrap-demo','/session', 'controllers/session/destroy.php')->only('auth');
 
 
